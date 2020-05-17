3eb7252f37107c98e840a0bf452b69a1fcdb82ae
Muthia19 <64302050+Muthia19@users.noreply.github.com>
2020-05-18 05:41:32 +0700

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_UAS
+{
+    class Person 
+    {
+        // ini adalah atribut untuk Person
+        private string noKtp;
+        private string nama;
+        private string alamat;
+
+        #region **
+        // **. Buatkan Properties untuk setiap atribut
+        // ........................... YOUR CODE IS HERE ------------- 
+        public string NoKtp 
+        {
+            get { return this.noKtp; }
+            set { this.noKtp = value; }
+        }
+        public string Nama
+        {
+            get { return this.nama; }
+            set { this.nama = value; }
+        }
+        public string Alamat
+        {
+            get { return this.alamat; }
+            set { this.alamat = value; }
+        }
+        #endregion
+    }
+}
---
+++ Teller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_UAS
+{
+    #region **
+    // Teller diturunkan dari class Person 
+    #endregion
+    class Teller : Person
+    {
+        private string kodePegawai;
+        private string nomorTeller;
+
+        #region **
+        // **. Buat PROPERTIES dari atribut private Teller
+        // ........................... YOUR CODE IS HERE ------------- 
+        public string KodePegawai 
+        { 
+            get {return this.kodePegawai;}
+            set { this.kodePegawai = value; }
+        }
+        public string NomorTeller
+        {
+            get { return this.nomorTeller; }
+            set { this.nomorTeller = value; }
+        }
+        #endregion
+
+        #region **
+        // **. Buat constructor untuk teller
+        // parameter = nomorKTP, KodePegawai, nama, alamat, nomorteller
+        
+        public Teller(string _noKTP, string _nama, string _alamat, string _kodPegawai, string _nomorTeller)
+        {
+            // ........................... YOUR CODE IS HERE ------------- 
+            this.NoKtp = _noKTP;
+            this.Nama = _nama;
+            this.Alamat = _alamat;
+            this.kodePegawai = _kodPegawai;
+            this.nomorTeller = _nomorTeller;  
+        }
+        #endregion
+    }
+}
